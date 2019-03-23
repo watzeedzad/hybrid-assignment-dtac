9b5af78f9c6c5badd809cfd2ed1c64ba338bd321
Jirawat Boonsumret <jirawat859@hotmail.com>
2019-03-23 22:55:02 +0700

    [add] add planed components folder /w dummy files

--- App.js
+++ App.js
@@ -14,8 +14,6 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    flexDirection: 'column'
+  }
 });
