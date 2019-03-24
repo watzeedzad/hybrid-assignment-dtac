17a91bd219b3b2ab2ebc3ccbb08c7b2f40531e05
Jirawat Boonsumret <jirawat859@hotmail.com>
2019-03-24 22:53:44 +0700

    [add] apply font, add TitleBar and NavBar

--- components/Utils/NavBar.js
+++ components/Utils/NavBar.js
@@ -1,13 +1,185 @@
 //import liraries
 import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity, Image, TouchableHighlight } from 'react-native';
+import { Font, AppLoading } from 'expo';
+
+let tintColorText = '';
 
 // create a component
 class NavBar extends Component {
+
+    constructor(props) {
+      super(props)
+    
+      this.state = {
+         fontLoaded: false,
+         usageNavStatus: true,
+         packageNavStatus: false,
+         rewardNavStatus: false,
+         campaignNavStatus: false,
+         otherNavStatus: false
+      };
+    };
+
+    _onPressUsage = () => {
+        this.setState({
+            usageNavStatus: true
+        });
+        this.setState({
+            packageNavStatus: false
+        });
+        this.setState({
+            rewardNavStatus: false
+        });
+        this.setState({
+            campaignNavStatus: false
+        });
+        this.setState({
+            otherNavStatus: false
+        });
+    }
+
+    _onPressPackage = () => {
+        this.setState({
+            usageNavStatus: false
+        });
+        this.setState({
+            packageNavStatus: true
+        });
+        this.setState({
+            rewardNavStatus: false
+        });
+        this.setState({
+            campaignNavStatus: false
+        });
+        this.setState({
+            otherNavStatus: false
+        });
+    }
+
+    _onPressReward = () => {
+        this.setState({
+            usageNavStatus: false
+        });
+        this.setState({
+            packageNavStatus: false
+        });
+        this.setState({
+            rewardNavStatus: true
+        });
+        this.setState({
+            campaignNavStatus: false
+        });
+        this.setState({
+            otherNavStatus: false
+        });
+    }
+
+    _onPressCampaign = () => {
+        this.setState({
+            usageNavStatus: false
+        });
+        this.setState({
+            packageNavStatus: false
+        });
+        this.setState({
+            rewardNavStatus: false
+        });
+        this.setState({
+            campaignNavStatus: true
+        });
+        this.setState({
+            otherNavStatus: false
+        });
+    }
+
+    _onPressOther = () => {
+        this.setState({
+            usageNavStatus: false
+        });
+        this.setState({
+            packageNavStatus: false
+        });
+        this.setState({
+            rewardNavStatus: false
+        });
+        this.setState({
+            campaignNavStatus: false
+        });
+        this.setState({
+            otherNavStatus: true
+        });
+    }
+
+    async componentDidMount() {
+        styles.pressedNav = null;
+        await Font.loadAsync({
+            'niramit-regular': require('../../assets/fonts/Niramit-Regular.ttf')
+        });
+        this.setState({fontLoaded: true});
+    }
+
     render() {
+        if (!this.state.fontLoaded) {
+            return <AppLoading/>;
+        }
+        if (this.state.usageNavStatus) {
+            tintColorText = '#1CABF8';
+        } else {
+            tintColorText = '#2E353A'
+        }
         return (
             <View style={styles.container}>
-                <Text>NavBar</Text>
+                <View style={styles.usageNav}>
+                    <TouchableOpacity
+                        onPress={this._onPressUsage}
+                    >
+                        <View style={styles.iconPicContainer}>
+                            <Image style={[styles.iconPic, getTintColor(this.state.usageNavStatus)]} source={require('../../assets/utils/usage.png')}/>
+                        </View>
+                        <Text style={[styles.text, getTextColor(this.state.usageNavStatus)]}> Usage </Text>
+                    </TouchableOpacity>
+                </View>
+                <View style={styles.packageNav}>
+                    <TouchableOpacity
+                        onPress={this._onPressPackage}
+                    >
+                        <View style={styles.iconPicContainer}>
+                            <Image style={[styles.iconPic, getTintColor(this.state.packageNavStatus)]} source={require('../../assets/utils/package.png')}/>
+                        </View>
+                        <Text style={[styles.text, getTextColor(this.state.packageNavStatus)]}> Package </Text>
+                    </TouchableOpacity>
+                </View>
+                <View style={styles.rewardNav}>
+                    <TouchableOpacity
+                        onPress={this._onPressReward}
+                    >
+                        <View style={styles.iconPicContainer}>
+                            <Image style={[styles.iconPic, getTintColor(this.state.rewardNavStatus)]} source={require('../../assets/utils/reward.png')}/>
+                        </View>
+                        <Text style={[styles.text, getTextColor(this.state.rewardNavStatus)]}> Reward </Text>
+                    </TouchableOpacity>
+                </View>
+                <View style={styles.campaignNav}>
+                    <TouchableOpacity
+                        onPress={this._onPressCampaign}
+                    >
+                        <View style={styles.iconPicContainer}>
+                            <Image style={[styles.iconPic, getTintColor(this.state.campaignNavStatus)]} source={require('../../assets/utils/campaign.png')}/>
+                        </View>
+                        <Text style={[styles.text, getTextColor(this.state.campaignNavStatus)]}> Campaign </Text>
+                    </TouchableOpacity>
+                </View>
+                <View style={styles.otherNav}>
+                    <TouchableOpacity
+                        onPress={this._onPressOther}
+                    >
+                        <View style={styles.iconPicContainer}>
+                            <Image style={[styles.iconPic, getTintColor(this.state.otherNavStatus)]} source={require('../../assets/utils/other.png')}/>
+                        </View>
+                        <Text style={[styles.text]}> Other </Text>
+                    </TouchableOpacity>
+                </View>
             </View>
         );
     }
@@ -16,12 +188,76 @@ class NavBar extends Component {
 // define your styles
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
+        flex: 0.1,
+        flexDirection: 'row',
+    },
+    usageNav: {
+        flex: 0.2,
+        flexDirection: 'column',
         justifyContent: 'center',
         alignItems: 'center',
-        backgroundColor: '#2c3e50',
+    },
+    packageNav: {
+        flex: 0.2,
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    rewardNav: {
+        flex: 0.2,
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    campaignNav: {
+        flex: 0.2,
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    otherNav: {
+        flex: 0.2,
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    text: {
+        fontFamily: 'niramit-regular',
+        fontSize: 14,
+    },
+    iconPicContainer: {
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    iconPic: {
+        width: 25,
+        height: 25,
     },
 });
+
+function getTintColor(isPress) {
+    if (isPress) {
+        return {
+            tintColor: '#1CABF8'
+        }
+    } else {
+        return {
+            tintColor: '#2E353A'
+        }
+    }
+}
+
+function getTextColor(isPress) {
+    if (isPress) {
+        return {
+            color: '#1CABF8'
+        }
+    } else {
+        return {
+            color: '#2E353A'
+        }
+    }
+}
 
 //make this component available to the app
 export default NavBar;
--- components/Utils/TitleBar.js
+++ components/Utils/TitleBar.js
@@ -1,13 +1,39 @@
 //import liraries
 import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Image, StatusBar } from 'react-native';
+import { Font, AppLoading } from 'expo';
 
 // create a component
 class TitleBar extends Component {
+
+    constructor(props) {
+      super(props)
+    
+      this.state = {
+         fontLoaded: false
+      };
+    };
+    
+
+    async componentWillMount() {
+        await Font.loadAsync({
+            'niramit-regular': require('../../assets/fonts/Niramit-Regular.ttf')
+        });
+        this.setState({fontLoaded: true});
+    }
+
     render() {
+        if (!this.state.fontLoaded) {
+            return <AppLoading/>;
+        }
         return (
             <View style={styles.container}>
-                <Text>TitleBar</Text>
+                <View style={styles.subContainerLeft}>
+                    <Text style={styles.titleText}> {this.props.titleText} </Text>
+                </View>
+                <View style={styles.subContainerRight}>
+                    <Image style={styles.dtacIcon} source={require('../../assets/utils/dtac-logo.png')}/>
+                </View>
             </View>
         );
     }
@@ -16,11 +42,31 @@ class TitleBar extends Component {
 // define your styles
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#2c3e50',
+        flex: 0.1,
+        flexDirection: 'row',
+        backgroundColor: '#19AAF8',
+        marginTop: StatusBar.currentHeight,
     },
+    subContainerLeft: {
+        flex: 0.5,
+        flexDirection: 'row'
+    },
+    subContainerRight: {
+        flex: 0.5,
+        flexDirection: 'row-reverse'
+    },
+    titleText: {
+        fontFamily: 'niramit-regular',
+        fontSize: 18,
+        color: '#FFFFFF',
+        margin: 15,
+    },
+    dtacIcon: {
+        width: 30,
+        height: 30,
+        resizeMode: 'contain',
+        margin: 15,
+    }
 });
 
 //make this component available to the app
