b2497038867183d2e8a33e0c43351a9b6184bd20
gigamanas <frest2013@gmail.com>
2019-03-24 21:48:27 +0700

    [Add]เพิ่มหน้า Campaign

--- App.js
+++ App.js
@@ -1,19 +1,11 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Campaign from './pages/Campaign'
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+      <Campaign />
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: 'column'
-  }
-});
